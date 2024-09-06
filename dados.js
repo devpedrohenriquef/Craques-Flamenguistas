17e2017d9cc7a72cecc0eadfb17054c943f26265
devpedrin <devph.frontend@gmail.com>
2024-09-06 11:47:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let jogadores = [
+  {
+    titulo: "Vinícius Junior",
+    descricao:
+      "Vinícius Júnior, também conhecido também como Vini Jr, é um jovem e promissor atacante brasileiro que atua pelo Real Madrid. Revelado nas categorias de base do Flamengo, Vinícius Júnior se transferiu para o Real Madrid com 16 anos e rapidamente se tornou uma das maiores promessas do futebol mundial. Conhecido por sua velocidade, dribles e habilidades de finalização, Vini Jr. é o destaque do Real Madrid e será o melhor jogador do MUNDO em breve.. ",
+    link: "https://pt.wikipedia.org/wiki/Vinícius_Júnior",
+  },
+  {
+    titulo: "Neymar Jr",
+    descricao:
+      "Neymar da Silva Santos Júnior, mais conhecido como Neymar, é um futebolista brasileiro que atua como atacante. Considerado um dos melhores jogadores do mundo, Neymar ganhou destaque no Santos, onde foi artilheiro e campeão da Copa Libertadores, antes de se transferir para o Barcelona, onde conquistou diversos títulos, incluindo a Liga dos Campeões da UEFA. Atualmente, joga no Paris Saint-Germain.",
+    link: "https://pt.wikipedia.org/wiki/Neymar",
+  },
+  {
+    titulo: "Gabriel Barbosa",
+    descricao:
+      "Gabriel Barbosa Almeida, mais conhecido como Gabigol, é um futebolista brasileiro que atua como atacante. Revelado nas categorias de base do Santos, Gabigol conquistou diversos títulos, incluindo a Copa Libertadores da América de 2011. Atualmente, joga pelo Flamengo.",
+    link: "https://pt.wikipedia.org/wiki/Gabriel_Barbosa",
+  },
+];
